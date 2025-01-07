59da05efb10c65783ff796b929ec269b8425255f
Lailaebrahim <ebrahimlaila815@gmail.com>
2025-01-07 21:37:51 +0200

    Chat Object Model

---
+++ backend/src/models/Chat.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+export const chatSchema = new Schema({
+  sessionId: {
+    type: String,
+    required: true,
+  },
+  messages: [
+    {
+      userMessage: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+      },
+      botMessage: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Chat = model("chat", chatSchema);
+export default Chat;
